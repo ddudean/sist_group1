4b3b1011334743cfef0c0fd0a663e4d6adb35fe5
DDUDEAN <DDUDEAN@DESKTOP-TQOPU2H>
2018-06-14 11:36:17 +0900

    하~~~~~잇!

--- group1/src/kr/co/sist/user/DDuDean.java
+++ group1/src/kr/co/sist/user/DDuDean.java
@@ -4,7 +4,7 @@ public class DDuDean {
 
 	public static void main(String[] args) {
 		System.out.println("하이 헬로~~~~~");
-		System.out.println("하이 헬로입니다.~~~~~");
+		System.out.println("하이 헬로입니다~~~~~");
 	}
 
 }
