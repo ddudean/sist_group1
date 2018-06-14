9a694e87579f6628d1546d81980394d1fe011a28
박지영 <@jiyoung>
2018-06-14 12:26:32 +0900

    순서중요해!

--- group1/src/kr/co/sist/user/JiYoungPark.java
+++ group1/src/kr/co/sist/user/JiYoungPark.java
@@ -1,9 +1,10 @@
 package kr.co.sist.user;
 
 public class JiYoungPark {
-
+ 
 	public static void main(String[] args) {
 		System.out.println("박지영ㅇㅇㅇ");
+		System.out.println("목요일 금요일 토요일 일요일");
 	}
 
 }
