a6882d5e332eaf2ad07bd219ecaeb9d73852e142
trueyoung <trueyoung@TRUEYOUNG>
2018-06-14 11:35:51 +0900

    이번엔 되나

--- group1/src/kr/co/sist/user/Trueyoung.java
+++ group1/src/kr/co/sist/user/Trueyoung.java
@@ -3,6 +3,6 @@ package kr.co.sist.user;
 public class Trueyoung {
 	public static void main(String args[]) {
 		System.out.println("TRUEYOUNG");
-		System.out.println("TRUEYOUNG!");
+		System.out.println("TRUEYOUNG!!!");
 	}
 }
