8ea1c763421ab0fdbb6cfaaa7f18cdfde2cbc384
trueyoung <trueyoung@TRUEYOUNG>
2018-06-14 12:26:59 +0900

    브랜치 커밋

--- group1/src/kr/co/sist/user/Trueyoung.java
+++ group1/src/kr/co/sist/user/Trueyoung.java
@@ -4,5 +4,7 @@ public class Trueyoung {
 	public static void main(String args[]) {
 		System.out.println("TRUEYOUNG");
 		System.out.println("TRUEYOUNG!!!");
+		System.out.println("안되는데 머지가");
+		
 	}
 }
